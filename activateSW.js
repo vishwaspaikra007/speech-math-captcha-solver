b9f46da68ac1c6692841b627599ee5d86f205f27
Vishwas Paikra <vishwaspaikra@gmail.com>
2022-11-13 13:20:33 +0530

    scope change

--- activateSW.js
+++ activateSW.js
@@ -2,7 +2,7 @@ const registerServiceWorker = async () => {
     if ("serviceWorker" in navigator) {
       try {
         const registration = await navigator.serviceWorker.register("./serviceWorkerSCS.js", {
-          scope: "/",
+          scope: "/speech-math-captcha-solver",
         });
         if (registration.installing) {
           console.log("Service worker installing");
